bf1eee16eb3eeac78a3c47f181ee2a1094e4cb57
Ángel Otero Barreiros <90766774+angelbarreiros@users.noreply.github.com>
2023-04-01 17:14:30 +0200

    Add files via upload

---
+++ Softdle/Backend/softdle/src/main/java/backend/softdle/SoftdleApplication.java
@@ -0,0 +1,13 @@
+package backend.softdle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftdleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftdleApplication.class, args);
+	}
+
+}
